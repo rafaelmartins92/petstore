91c3d3df09cbb61a5359db7f10803ae54574a22d
Rafael Martins <rafael.martins92@outlook.com>
2021-11-08 01:47:22 -0300

    feat: add back to top and whatsapp buttons

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// BACK TO TOP
+const backToTopButton = document.querySelector(".back-to-top");
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+// When Scroll
+window.addEventListener("scroll", function () {
+  backToTop();
+});
